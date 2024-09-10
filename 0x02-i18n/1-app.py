56b64f078c811e144d1a8d4d7e5c51c15ae01906
mohamedRajab0 <elzimmohamed@gmail.com>
2024-09-10 17:22:44 +0300

    2c

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,6 +5,21 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Configuration class for the application.
+        Attributes:
+        Languages (list): List of supported languages.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app.config.from_object(Config)
 
 
 @app.route("/")
@@ -14,7 +29,7 @@ def hello():
     Returns:
         The rendered template for the index.html page.
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
